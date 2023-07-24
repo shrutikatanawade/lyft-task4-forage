b5f0ea14c465498b552f30ce84b9a435d6344c70
shrutikatanawade <110730111+shrutikatanawade@users.noreply.github.com>
2023-07-24 13:29:16 +0530

    car.py

--- car.py
+++ car.py
@@ -11,3 +11,17 @@ class Car:
 
     def needs_service(self):
         return self.engine.needs_service() or self.battery.needs_service()
+
+class Tire:
+    def __init__(self, tire_type):
+        self.type = tire_type
+
+    def needs_service(self, tire_wear_array):
+        if self.type == "Carrigan":
+            # Carrigan tires need service if any value in the tire wear array is >= 0.9
+            return any(wear >= 0.9 for wear in tire_wear_array)
+        elif self.type == "Octoprime":
+            # Octoprime tires need service if the sum of all values in the tire wear array is >= 3
+            return sum(tire_wear_array) >= 3
+        else:
+            raise ValueError("Invalid tire type")
